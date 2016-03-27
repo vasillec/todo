ab8ed7b1d3802df9f2ce85b129711a3b20e405f8
vasillec <vasillec@mail.ru>
2016-03-27 23:32:40 +0300

    Update registration.php

--- view/registration.php
+++ view/registration.php
@@ -1,13 +1,12 @@
-<script src="view/js/script.js"></script>
 <script src="view/js/jquery.validate.min.js"></script>
-<script src="view/js/validation.js"></script>
+<script src="view/js/script.js"></script>
 <section class="main-form-wrap">
       <h1 class="main-title">Registration</h1>
-      <form class="auth-fotm fotm-style" role="form"  method="post">
+      <form id="reg-form" class="auth-fotm fotm-style" role="form"  method="post">
          <div><input id="login" name="login" type="text" class="auth-form-login" placeholder="Login"></div>
-         <div><input id="password" name="password" type="password" placeholder="Password"><br></div>
-         <div><input id="password2" name="password2" type="password" placeholder="Confirm password"><br></div>
-         <input class="blue-btn" id="reg_btn" type="button" value="Submit">
+         <div><input id="password" name="password" type="password" placeholder="Password"></div>
+         <div><input id="password2" name="password2" type="password" placeholder="Confirm password"></div>
+         <input class="blue-btn" id="reg_btn" type="submit" value="Submit">
          <a href="auth.php">Sign In</a>
      </form>
  </section>
