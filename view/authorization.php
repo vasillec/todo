302b83d0751daee2b72aa403447ed2efa6966d67
vasillec <vasillec@mail.ru>
2016-03-27 23:30:37 +0300

    Update authorization.php

--- view/authorization.php
+++ view/authorization.php
@@ -1,10 +1,11 @@
+<script src="view/js/jquery.validate.min.js"></script>
 <script src="view/js/script.js"></script>
 <section class="main-form-wrap">
   <h1 class="main-title">Authorization</h1>
-  <form class="auth-fotm fotm-style" role="form"  method="post">
+  <form id="auth-fotm" class="auth-fotm fotm-style" role="form"  method="post">
      <div><input id="login" name="login" type="text" class="auth-form-login" placeholder="Login"></div>
-     <div><input id="password" name="password" type="password" placeholder="Password"><br></div>
-     <input class="blue-btn" id="login_btn" type="button" value="Sign in">
+     <div><input id="password" name="password" type="password" placeholder="Password"></div>
+     <input class="blue-btn" id="login_btn" type="submit" value="Sign in">
      <a href="reg.php">Sign Up</a>
  </form>
 </section>
