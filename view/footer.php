4b13ed34657574cf8feb9735af927ff089bf29a0
vasillec <vasillec@mail.ru>
2016-03-27 23:31:14 +0300

    Update footer.php

--- view/footer.php
+++ view/footer.php
@@ -1,8 +1,14 @@
 <?php if(isset($_SESSION["logged_in"])) : ?>
-    </section>
-    <footer>
-        <button id="addTodoList" type="button" class="blue-btn" onclick="Add_project();"><span>Add TODO List</span></button>
-    </footer>
+</section>
+<footer>
+	<button id="addTodoList" type="button" class="blue-btn" onclick="Add_project();"><span>Add TODO List</span></button>
+</footer>
 <?php endif; ?>
+<div id="errorBlock" class="errorBlock">
+	<span class="exit" title="close">✖</span>
+	<span>ERROR! </span>
+	<span class="error-message"></span>
+</div>
+<div id="hide-layout" class="hide-layout"></div>
 </body>
 </html>
